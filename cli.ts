3a756d84d0b2f21ce95179e7ff335a73f8670549
Tristan <LeoDog896@hotmail.com>
2022-07-04 15:07:31 -0400

    Ask for empty directory

--- cli.ts
+++ cli.ts
@@ -1,20 +1,28 @@
 import { types as createTypes } from "./src/create/mod.ts"
 import { join } from "https://deno.land/std@0.146.0/path/mod.ts";
-import { green, red } from "./src/theme.ts"
+import { blue, green, red } from "./src/theme.ts"
 import { Confirm } from "https://deno.land/x/cliffy@v0.24.2/prompt/mod.ts";
 import { Command, EnumType, CompletionsCommand, HelpCommand } from "https://deno.land/x/cliffy@v0.24.2/command/mod.ts";
 
 async function askForEmpty(directory: string) {
   try {
+
+    let found = false
+
     for await (const file of Deno.readDir(directory)) {
       if (file.isFile) {
-        const shouldContinue = await Confirm.prompt(`Directory is not empty. Continue?`);
+        found = true;
+        break;
+      }
+    }
 
-        if (shouldContinue) {
-          return;
-        } else {
-          Deno.exit(0)
-        }
+    if (found) {
+      const shouldContinue = await Confirm.prompt(`Directory is not empty. Continue?`);
+
+      if (shouldContinue) {
+        return;
+      } else {
+        Deno.exit(0)
       }
     }
   } catch {
@@ -28,8 +36,11 @@ const startCommand = new Command()
   .name("create")
   .description("Create a new project with a type.")
   .type("createType", createType)
-  .arguments("<type:createType> <directory:file>")
-  .action(async (_, type, directory) => {
+  .arguments("<type:createType> [directory:file]")
+  .action(async (_, type, directory = "./") => {
+
+    // Get the name of the directory (if it's ./, itll be the name of the last directory in the name)
+    const directoryName = directory == "./" ? Deno.cwd().split("/").pop()! : directory;
 
     await askForEmpty(directory)
 
@@ -39,7 +50,7 @@ const startCommand = new Command()
       await Deno.writeTextFile(join(directory, file.path), file.content, { create: true })
     })
 
-    console.log(`${green(`Project ${directory} created!`)}`)
+    console.log(`${green(`Project ${blue(directoryName)} created!`)}`)
   });
 
 const main = new Command()
