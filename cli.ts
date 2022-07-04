b0696aa7ed95e8237dec4f248498515b0cac1f94
Tristan <LeoDog896@hotmail.com>
2022-07-04 13:39:49 -0400

    Add theeming

--- cli.ts
+++ cli.ts
@@ -1,6 +1,7 @@
 import { types as createTypes } from "./src/create/mod.ts"
 import { parse } from "https://deno.land/std@0.146.0/flags/mod.ts";
 import { join } from "https://deno.land/std@0.146.0/path/mod.ts";
+import { red } from "./src/theme.ts"
 
 const rawArgs = parse(Deno.args)
 
@@ -12,17 +13,17 @@ const args = {
 if (args._[0] == "create") {
   const type = args._[1]
   
-  if (type !in createTypes) {
+  if (type in createTypes === false) {
     if (type == undefined) {
       console.error(`Please specify a type: (${Object.keys(createTypes).join(", ")})`)
     } else {
-      console.error(`Unknown type: ${type}`)
+      console.error(`${red("Unknown type:")} ${type}`)
     }
 
     Deno.exit(1)
   }
 
-  const directory = args._[2] || "."
+  const directory = args._[2] || "./"
 
   createTypes[type]?.files?.forEach(async file => {
     await Deno.writeTextFile(join(directory, file.path), file.content)
---
+++ src/theme.ts
@@ -0,0 +1,1 @@
+export { green, red, blue } from "https://deno.land/std@0.146.0/fmt/colors.ts"
