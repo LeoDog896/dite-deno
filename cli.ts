ce90c6e3c5ce508edf5aa49d604a835d0f774da1
Tristan <LeoDog896@hotmail.com>
2022-07-04 14:00:10 -0400

    Ask before putting files if the folder isn't empty

--- cli.ts
+++ cli.ts
@@ -1,7 +1,26 @@
 import { types as createTypes } from "./src/create/mod.ts"
 import { parse } from "https://deno.land/std@0.146.0/flags/mod.ts";
 import { join } from "https://deno.land/std@0.146.0/path/mod.ts";
-import { red } from "./src/theme.ts"
+import { red, green, blue } from "./src/theme.ts"
+import { Confirm } from "https://deno.land/x/cliffy@v0.24.2/prompt/mod.ts";
+
+async function askForEmpty(directory: string) {
+  try {
+    for await (const file of Deno.readDir(directory)) {
+      if (file.isFile) {
+        const shouldContinue = await Confirm.prompt(`Directory is not empty. Continue?`);
+
+        if (shouldContinue) {
+          return;
+        } else {
+          Deno.exit(0)
+        }
+      }
+    }
+  } catch {
+    void 0;
+  }
+}
 
 const rawArgs = parse(Deno.args)
 
@@ -25,9 +44,13 @@ if (args._[0] == "create") {
 
   const directory = args._[2] ?? "./"
 
+  askForEmpty()
+
   createTypes[type]?.files?.forEach(async file => {
     // make any necessary directories THEN create the file
     await Deno.mkdir(join(directory, file.path.split("/").slice(0, -1).join("/")), { recursive: true })
     await Deno.writeTextFile(join(directory, file.path), file.content, { create: true })
   })
+
+  console.log(`${green(`Project ${(blue(args._[2] + " ") ?? "")}created!`)}`)
 }
