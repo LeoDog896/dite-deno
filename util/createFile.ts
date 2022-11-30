f4b03b8bea838e19d590ececea71ee07ca744f0b
Tristan F <LeoDog896@hotmail.com>
2022-11-30 18:15:21 +0000

    Don't update lock, better docs

--- util/createFile.ts
+++ util/createFile.ts
@@ -1,5 +1,10 @@
 import { join } from "../import/path.ts";
 
+/**
+ * Small utility to create a new file given JSON data
+ * @param file The file to put. This can take `/`s, such as "folder/index.html"
+ * @param directory The **working directory**. Not to be confused with the directory name to put the file in.
+ */
 export const createFile = async (
   file: { path: string; content: string },
   directory = Deno.cwd(),
