13fbb05fc0bc3a301f05c9e9fd1e9f7609c3e8d2
Tristan F <LeoDog896@hotmail.com>
2022-12-03 11:56:02 -0500

    bump esbuild_plugin_deno_resolve

--- src/esbuild/deno-resolve.ts
+++ src/esbuild/deno-resolve.ts
@@ -1,1 +1,1 @@
-export { default } from "https://deno.land/x/esbuild_plugin_deno_resolve@v0.0.7/index.ts";
+export { default } from "https://deno.land/x/esbuild_plugin_deno_resolve@v0.0.8/index.ts";
